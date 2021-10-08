0414be1778d162a6dbb0818c88089d04506395d8
Harcourt <harcourtbullv@gmail.com>
2021-10-08 10:51:30 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const step1 = document.querySelector( '#step-1 img' );
+const map = document.querySelector( '.map' );
+
+window.addEventListener( 'resize', () => {
+    // step1.style.transform = `translateY( -${ window.innerHeight * 0.024 }px )`;
+    // step1.style.top = `${ window.innerHeight * 0.03 }%`;
+    // console.log( 'Window: ' + window.innerHeight );
+    // console.log( 'Map: ' + map.offsetHeight );
+});
